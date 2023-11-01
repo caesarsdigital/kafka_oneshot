929c7f70427d56bd422a62dcf220a9dd4a1429ea
Brandon Barker <brandon.barker@gmail.com>
2023-11-01 17:23:40 -0400

    add arguments and key support

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,58 @@
+use clap::Parser;
 use rdkafka::config::ClientConfig;
 use rdkafka::producer::{FutureProducer, FutureRecord};
+use serde_json::Value;
 use std::io::{self, BufRead};
 use std::time::Duration;
 use tokio;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(long, default_value = "localhost:9092")]
+    server: String,
+    #[arg(long)]
+    key: String,
+    #[arg(long)]
+    topic: String,
+}
+
 #[tokio::main]
 async fn main() {
-    // Parse command-line arguments
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Usage: {} <kafka_topic>", args[0]);
-        std::process::exit(1);
-    }
-    let topic = &args[1];
+    let opts: Cli = Cli::parse();
 
-    // Set up Kafka producer
     let producer: FutureProducer = ClientConfig::new()
-        .set("bootstrap.servers", "localhost:9092")
+        .set("bootstrap.servers", &opts.server)
         .create()
         .expect("Producer creation error");
 
-    // Read JSON messages from STDIN and publish to Kafka
     let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let message = line.expect("Failed to read line from STDIN");
-        let _ = producer
-            .send(
-                FutureRecord::to(topic)
-                    .payload(&message)
-                    .key(""),
-                Some(Duration::from_secs(10)), // 10 seconds timeout
-            )
-            .await
-            .expect("Failed to send message to Kafka");
+    let lines: Vec<_> = stdin.lock().lines().collect::<Result<_, _>>().unwrap();
+
+    for line in lines {
+        if let Some(key) = extract_key_from_json(&line, &opts.key) {
+            let _ = producer.send(
+                FutureRecord::to(&opts.topic)
+                    .payload(&line)
+                    .key(&key),
+                Duration::from_secs(10),
+            ).await.expect("Failed to send message to Kafka");
+        }
     }
+}
+
+fn extract_key_from_json(json_str: &str, pointer: &str) -> Option<String> {
+    let json: Value = serde_json::from_str(json_str).ok()?;
+    json_pointer_to_value(&json, pointer)
+        .and_then(|v| v.as_str())
+        .map(String::from)
+}
+
+fn json_pointer_to_value<'a>(json: &'a Value, pointer: &str) -> Option<&'a Value> {
+    pointer.split('.').fold(Some(json), |acc, part| {
+        acc.and_then(|v| match v {
+            Value::Object(map) => map.get(part),
+            _ => None,
+        })
+    })
 }
