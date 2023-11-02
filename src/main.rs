5dd1f205f2f81efb764a5922f3d123c6b8fdc66b
Brandon Barker <brandon.barker@gmail.com>
2023-11-02 10:48:14 -0400

    convert main loop to a stream

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![deny(warnings)]
 
 use clap::Parser;
+use futures::StreamExt;
 use rdkafka::config::ClientConfig;
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use serde_json::Value;
 use std::io::{self, BufRead};
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
 #[derive(Parser)]
@@ -22,46 +24,50 @@ struct Cli {
 async fn main() {
     let opts: Cli = Cli::parse();
 
-    let producer: FutureProducer = ClientConfig::new()
-        .set("bootstrap.servers", &opts.server)
-        .create()
-        .expect("Producer creation error");
+    let producer: Arc<Mutex<FutureProducer>> = Arc::new(Mutex::new(
+        ClientConfig::new()
+            .set("bootstrap.servers", &opts.server)
+            .create()
+            .expect("Producer creation error"),
+    ));
 
     let stdin = io::stdin();
     let reader = stdin.lock();
-    let mut buffer = String::new();
-    let mut brace_count = 0;
 
-    for line in reader.lines() {
-        let line = line.unwrap();
-        for ch in line.chars() {
-            buffer.push(ch);
+    let _ = futures::stream::iter(reader.lines())
+        .map(|line| line.unwrap())
+        .flat_map(|line| futures::stream::iter(line.chars().collect::<Vec<char>>()))
+        .fold((String::new(), 0), move |(mut buffer, brace_count), ch| {
+            let key_path = opts.key.clone();
+            let producer_clone = producer.clone();
+            let topic = opts.topic.clone();
+            async move {
+                buffer.push(ch);
+                let brace_count = match ch {
+                    '{' => brace_count + 1,
+                    '}' => brace_count - 1,
+                    _ => brace_count,
+                };
 
-            // Count opening and closing braces
-            if ch == '{' {
-                brace_count += 1;
-            } else if ch == '}' {
-                brace_count -= 1;
-            }
-
-            // If braces are balanced, process the JSON object
-            if brace_count == 0 && !buffer.trim().is_empty() {
-                if let Some(key) = extract_key_from_json(&buffer, &opts.key) {
-                    //println!("sending {} with key: {}", &buffer, &key);
-                    let _ = producer
-                        .send(
-                            FutureRecord::to(&opts.topic).payload(&buffer).key(&key),
-                            Duration::from_secs(10),
-                        )
-                        .await
-                        .expect("Failed to send message to Kafka");
+                if brace_count == 0 && !buffer.trim().is_empty() {
+                    if let Some(key) = extract_key_from_json(&buffer, &key_path) {
+                        let producer_lock = producer_clone.lock().unwrap();
+                        let _ = producer_lock
+                            .send(
+                                FutureRecord::to(&topic).payload(&buffer).key(&key),
+                                Duration::from_secs(10),
+                            )
+                            .await
+                            .expect("Failed to send message to Kafka");
+                    }
+                    buffer.clear();
                 }
-                buffer.clear();
-            }
-        }
-    }
-}
 
+                (buffer, brace_count)
+            }
+        })
+        .await;
+}
 fn extract_key_from_json(json_str: &str, pointer: &str) -> Option<String> {
     let json: Value = serde_json::from_str(json_str).ok()?;
     json_pointer_to_value(&json, pointer)
