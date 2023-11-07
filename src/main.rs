3f539c390725e140f0970e8e27e31aa5d3930ddf
Brandon Elam Barker <brandon.barker@gmail.com>
2023-11-07 09:08:25 -0500

    Add consumer (#3)
    * add consumer and option to run consumer and producer
    
    * fix some minor issues; update Readme

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,51 @@
 use clap::Parser;
 use futures::StreamExt;
 use rdkafka::config::ClientConfig;
+use rdkafka::consumer::{Consumer, StreamConsumer};
+use rdkafka::error::KafkaResult;
+use rdkafka::message::Message;
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use serde_json::Value;
 use std::io::{self, BufRead};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[derive(clap::ValueEnum, Clone)]
+enum Mode {
+    Producer,
+    Consumer,
+    Both,
+}
+
+#[derive(Clone, Parser)]
+#[command(author, version, about = "A simple pub/sub tool for Kafka", long_about = None)]
 struct Cli {
     #[arg(long, default_value = "localhost:9092")]
     server: String,
-    #[arg(long)]
-    key: String,
+    #[arg(value_enum, long, default_value_t = Mode::Producer)]
+    mode: Mode,
+    // The key field is only required if the mode includes Producer
+    #[arg(long, requires_ifs = [("Producer", "mode"), ("Both", "mode")])]
+    key: Option<String>,
     #[arg(long)]
     topic: String,
 }
 
-#[tokio::main]
-async fn main() {
-    let opts: Cli = Cli::parse();
+fn extract_key_from_json(json_str: &str, pointer: &str) -> Option<String> {
+    let json: Value = serde_json::from_str(json_str).ok()?;
+    json_pointer_to_value(&json, pointer)
+        .and_then(|v| v.as_str())
+        .map(String::from)
+}
 
+fn json_pointer_to_value<'a>(json: &'a Value, pointer: &str) -> Option<&'a Value> {
+    pointer.split('.').try_fold(json, |acc, part| match acc {
+        Value::Object(map) => map.get(part),
+        _ => None,
+    })
+}
+
+async fn run_producer(opts: Cli) -> KafkaResult<()> {
     let producer: Arc<Mutex<FutureProducer>> = Arc::new(Mutex::new(
         ClientConfig::new()
             .set("bootstrap.servers", &opts.server)
@@ -34,7 +58,7 @@ async fn main() {
     let stdin = io::stdin();
     let reader = stdin.lock();
 
-    let _ = futures::stream::iter(reader.lines())
+    futures::stream::iter(reader.lines())
         .map(|line| line.unwrap())
         .flat_map(|line| futures::stream::iter(line.chars().collect::<Vec<char>>()))
         .fold((String::new(), 0), move |(mut buffer, brace_count), ch| {
@@ -50,7 +74,9 @@ async fn main() {
                 };
 
                 if brace_count == 0 && !buffer.trim().is_empty() {
-                    if let Some(key) = extract_key_from_json(&buffer, &key_path) {
+                    if let Some(key) =
+                        extract_key_from_json(&buffer, &key_path.unwrap_or("id".to_string()))
+                    {
                         let producer_lock = producer_clone.lock().unwrap();
                         let _ = producer_lock
                             .send(
@@ -67,17 +93,55 @@ async fn main() {
             }
         })
         .await;
-}
-fn extract_key_from_json(json_str: &str, pointer: &str) -> Option<String> {
-    let json: Value = serde_json::from_str(json_str).ok()?;
-    json_pointer_to_value(&json, pointer)
-        .and_then(|v| v.as_str())
-        .map(String::from)
+    Ok(())
 }
 
-fn json_pointer_to_value<'a>(json: &'a Value, pointer: &str) -> Option<&'a Value> {
-    pointer.split('.').try_fold(json, |acc, part| match acc {
-        Value::Object(map) => map.get(part),
-        _ => None,
-    })
+async fn run_consumer(opts: Cli) -> KafkaResult<()> {
+    // Set up the Kafka consumer configuration
+    let consumer: StreamConsumer = ClientConfig::new()
+        .set("group.id", "kafka_oneshot")
+        .set("bootstrap.servers", &opts.server)
+        .set("enable.auto.commit", "true")
+        .set("auto.offset.reset", "earliest")
+        .create()?;
+
+    // Subscribe to the topic(s)
+    consumer.subscribe(&[&opts.topic])?;
+
+    // Create a stream to process messages
+    let message_stream = consumer.stream();
+
+    // Use `stream::for_each` to iterate over the messages asynchronously
+    message_stream
+        .for_each(|message| async {
+            match message {
+                Ok(msg) => {
+                    if let Some(payload) = msg.payload() {
+                        // Assuming the payload is a string, print it out
+                        println!(
+                            "Key: '{:?}', Payload: '{}'",
+                            msg.key().map(String::from_utf8_lossy),
+                            String::from_utf8_lossy(payload)
+                        );
+                    }
+                }
+                Err(e) => eprintln!("Kafka consumer error: {}", e),
+            }
+        })
+        .await;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let opts: Cli = Cli::parse();
+
+    match opts.mode {
+        Mode::Producer => run_producer(opts).await.unwrap(),
+        Mode::Consumer => run_consumer(opts).await.unwrap(),
+        Mode::Both => {
+            tokio::try_join!(run_producer(opts.clone()), run_consumer(opts.clone())).unwrap();
+        }
+    }
 }
