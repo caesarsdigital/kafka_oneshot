4cf60dfeffe641ceeb17ab9d64e488dbe9725b26
Brandon Elam Barker <brandon.barker@gmail.com>
2024-03-23 16:51:55 -0400

    use toml for rust-toolchain

--- shell.nix
+++ shell.nix
@@ -27,11 +27,11 @@
     ];
      # Only for building bracket-lib, I think:
     nativeBuildInputs = [ pkgs.pkg-config ];
-    RUSTC_VERSION = pkgs.lib.readFile ./rust-toolchain;
     # https://github.com/rust-lang/rust-bindgen#environment-variables
     LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
     HISTFILE = toString ./.history;
     shellHook = ''
+      export RUSTC_VERSION="$(grep 'channel =' rust-toolchain.toml | awk -F\" '{print $2}')"
       export PATH=''${CARGO_HOME:-~/.cargo}/bin:$PATH
       export PATH=''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/:$PATH
       '';
